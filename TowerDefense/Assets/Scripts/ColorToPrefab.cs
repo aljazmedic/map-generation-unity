1c8b57151f025d6a26ac4570d37ecb76e4f69c19
Aljaz Medic <aljaz.medic@gmail.com>
2018-05-12 21:01:55 +0200

    Unity Game Update
    Added psd file and its read2create map

---
+++ TowerDefense/Assets/Scripts/ColorToPrefab.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ColorToPrefab  {
+    public Color color;
+    public GameObject prefab;
+
+    ColorToPrefab(Color col, GameObject pref)
+    {
+        color = col;
+        prefab = pref;
+    }
+
+}
---
+++ TowerDefense/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public Texture2D map;
+    public ColorToPrefab[] Mappings;
+    public float scale;
+
+    void Start()
+    {
+        GameObject levelTiles = new GameObject("LevelTiles");
+        Instantiate(levelTiles);
+        GenerateLevel(levelTiles);
+    }
+
+    private void GenerateLevel(GameObject levelTiles)
+    {
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                GenerateTile(x, y, levelTiles);
+            }
+        }
+    }
+
+    void GenerateTile(int x, int y, GameObject parent){
+        Color pixelColor = map.GetPixel(x, y);
+
+        if (pixelColor.a == 0)
+        {
+            //transparent
+            return;
+        }
+
+        for (int i = 0; i < Mappings.Length; i++)
+        {
+            if (pixelColor.Equals(Mappings[i].color))
+            {
+                Instantiate(Mappings[i].prefab, new Vector3(x * scale, 0, y * scale), Quaternion.identity, parent.transform);
+                return;
+            }
+        }
+    }
+}
